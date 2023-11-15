e903d3347001670f207769f5ae623ea6c676fcdb
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-14 18:33:23 -0600

    UI initial cleanup
    Rock, Paper, Scissors buttons now change color depending on round
    outcome. Scoring visibility improved but not final.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const promptDiv = document.querySelector(".promptDiv");
 const btnDiv = document.querySelector(".btnDiv");
 const resultsDiv = document.querySelector(".resultsDiv");
-const gameOverDiv = document.querySelector(".gameOverDiv");
+const scoreDiv = document.querySelector(".scoreDiv");
 
 const playGameBtn = document.createElement("button");
 playGameBtn.textContent = "Click here to play Rock, Paper, Scissors";
@@ -19,19 +19,16 @@ let computerScore = 0;
 rockBtn.addEventListener("click", function (e) {
   playerChoice = "rock";
   playRound();
-  promptDiv.textContent = `Choose Rock, Paper, or Scissors.`;
 });
 
 paperBtn.addEventListener("click", function (e) {
   playerChoice = "paper";
   playRound();
-  promptDiv.textContent = `Choose Rock, Paper, or Scissors.`;
 });
 
 scissorsBtn.addEventListener("click", function (e) {
   playerChoice = "scissors";
   playRound();
-  promptDiv.textContent = `Choose Rock, Paper, or Scissors.`;
 });
 
 const getComputerChoice = () => {
@@ -58,52 +55,78 @@ const getComputerChoice = () => {
 const playRound = () => {
   let computerChoice = getComputerChoice();
 
+  let resultMsg;
+
+  paperBtn.style.backgroundColor = "";
+  rockBtn.style.backgroundColor = "";
+  scissorsBtn.style.backgroundColor = "";
+
   console.log(
     `Player plays: ${playerChoice}, computer plays: ${computerChoice}`
   );
   if (playerChoice == computerChoice) {
-    resultsDiv.textContent = `Game #${gameCounter}: Draw, try again!  Player: ${playerScore}, Computer: ${computerScore}`;
+    resultMsg = `Game #${gameCounter}: Draw, try again!`;
+    if (playerChoice === "rock") {
+      rockBtn.style.backgroundColor = "yellow";
+    } else if (playerChoice === "paper") {
+      paperBtn.style.backgroundColor = "yellow";
+    } else if (playerChoice === "scissors") {
+      scissorsBtn.style.backgroundColor = "yellow";
+    }
   } else if (playerChoice === "rock" && computerChoice === "paper") {
     computerScore++;
-    resultsDiv.textContent = `Game #${gameCounter}: You Lose! Paper beats Rock.   Player: ${playerScore}, Computer: ${computerScore}`;
+    rockBtn.style.backgroundColor = "red";
+    resultMsg = `Game #${gameCounter}: You Lose! Paper beats Rock.`;
   } else if (playerChoice === "paper" && computerChoice === "rock") {
     playerScore++;
-    resultsDiv.textContent = `Game #${gameCounter}: You Win! Paper beats Rock.  Player: ${playerScore}, Computer: ${computerScore}`;
+    paperBtn.style.backgroundColor = "green";
+    resultMsg = `Game #${gameCounter}: You Win! Paper beats Rock.`;
   } else if (playerChoice === "rock" && computerChoice === "scissors") {
     playerScore++;
-    resultsDiv.textContent = `Game #${gameCounter}: You Win! Rock beats Scissors.  Player: ${playerScore}, Computer: ${computerScore}`;
+    rockBtn.style.backgroundColor = "green";
+    resultMsg = `Game #${gameCounter}: You Win! Rock beats Scissors.`;
   } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    scissorsBtn.style.backgroundColor = "red";
     computerScore++;
-    resultsDiv.textContent = `Game #${gameCounter}: You Lose! Rock beats Scissors.  Player: ${playerScore}, Computer: ${computerScore}`;
+    resultMsg = `Game #${gameCounter}: You Lose! Rock beats Scissors.`;
   } else if (playerChoice === "scissors" && computerChoice === "paper") {
     playerScore++;
-    resultsDiv.textContent = `Game #${gameCounter}: You Win! Scissors beats Paper.  Player: ${playerScore}, Computer: ${computerScore}`;
+    scissorsBtn.style.backgroundColor = "green";
+    resultMsg = `Game #${gameCounter}: You Win! Scissors beats Paper.`;
   } else if (playerChoice === "paper" && computerChoice === "scissors") {
     computerScore++;
-    resultsDiv.textContent = `Game #${gameCounter}: You Lose! Scissors beats Paper.  Player: ${playerScore}, Computer: ${computerScore}`;
+    paperBtn.style.backgroundColor = "red";
+    resultMsg = `Game #${gameCounter}: You Lose! Scissors beats Paper.`;
   } else {
     return -1;
   }
+
+  
   gameCounter++;
 
-  if (gameCounter > 5) {
+  resultsDiv.textContent = resultMsg;
+  scoreDiv.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
+
+  if (playerScore >= 5 || computerScore >= 5) {
     gameOver();
     return;
   }
+
 };
 
 const playGame = () => {
   gameCounter = 1;
 
   playGameBtn.remove();
-  promptDiv.textContent = `Choose Rock, Paper, or Scissors.`;
+  resultsDiv.textContent = "";
+  scoreDiv.textContent = "";
 
   btnDiv.append(rockBtn);
   btnDiv.append(paperBtn);
   btnDiv.append(scissorsBtn);
-  rockBtn.textContent = "Rock";
-  paperBtn.textContent = "Paper";
-  scissorsBtn.textContent = "Scissors";
+  rockBtn.textContent = "\u{1FAA8}";
+  paperBtn.textContent = "\u{1F5DE}";
+  scissorsBtn.textContent = "\u{2704}";
 };
 
 playGameBtn.addEventListener("click", function (e) {
@@ -111,9 +134,15 @@ playGameBtn.addEventListener("click", function (e) {
 });
 
 const gameOver = () => {
-  promptDiv.append("GAME OVER");
+  playerScore = 0;
+  computerScore = 0;
   rockBtn.remove();
   paperBtn.remove();
   scissorsBtn.remove();
-  gameOverDiv.append(playGameBtn);
+  playGameBtn.textContent = "GAME OVER - Click here to play again";
+  promptDiv.append(playGameBtn);
+
+  paperBtn.style.backgroundColor = "";
+  rockBtn.style.backgroundColor = "";
+  scissorsBtn.style.backgroundColor = "";
 };
